56c068750c2e05a43a2315c33e38ed2cc37e5175
Sahibjeet <sahibjeetsingh234@gmail.com>
2025-04-23 12:30:06 +0530

    added js

---
+++ java.js
@@ -0,0 +1,38 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click',() =>{
+        nav.classList.add('active');
+    })
+    
+}
+if (close) {
+    close.addEventListener('click',() =>{
+        nav.classList.remove('active');
+    })
+    
+}
+  const toggle = document.getElementById("darkModeToggle");
+  const body = document.body;
+
+  // Apply saved theme on load
+  if (localStorage.getItem("theme") === "dark") {
+    body.classList.add("dark-mode");
+  }
+
+  // Toggle dark mode on button click
+  toggle.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+
+    // Save preference
+    if (body.classList.contains("dark-mode")) {
+      localStorage.setItem("theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+    }
+  });
+
+
+
